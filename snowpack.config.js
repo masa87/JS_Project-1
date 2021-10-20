3e828c949cb78cb821cc6d15ad9a7708ec383181
Tomek <61616164+masa87@users.noreply.github.com>
2021-10-20 21:17:57 +0200

    fetchAPI / load movies at first page

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,10 +1,13 @@
 // Snowpack Configuration File
 // See all supported options: https://www.snowpack.dev/reference/configuration
 
+const { fetchMovie } = require("./js/fetchMovie");
+
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
     /* ... */
+    fetchMovie,
   },
   plugins: [
     /* ... */
