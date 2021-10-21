de426036f32d47cdd427ab8a814a226a79e79e76
Tomek <61616164+masa87@users.noreply.github.com>
2021-10-21 09:20:29 +0200

    trend movies load at home page + bug fixes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-
-// import axios from "axios";
-// import { fetchMovie } from "./fetchMovie";
-
 const qs = (selector) => document.querySelector(selector);
 const inputTitle = qs(".header-input");
 const filmGallery = qs(".film-gallery");
@@ -17,7 +13,20 @@ async function fetchMovie(title) {
     if (!response.ok) {
       throw new Error(response.status);
     }
-    // console.log(response);
+    return await response.json();
+  } catch (err) {
+    return console.log(err);
+  }
+}
+
+async function fetchPopularMovie() {
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/trending/movie/week?api_key=b8c69e73ca2b06d4109ce06d6df842ad`
+    );
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
     return await response.json();
   } catch (err) {
     return console.log(err);
@@ -25,7 +34,7 @@ async function fetchMovie(title) {
 }
 
 const searchBoxValue = () => {
-  console.log(inputTitle.value);
+  // console.log(inputTitle.value);
   fetchMovie(inputTitle.value)
     .then((movie) => {
       console.log(movie);
@@ -36,8 +45,18 @@ const searchBoxValue = () => {
     });
 };
 
+const setPopularMovie = () => {
+  fetchPopularMovie()
+    .then((movie) => {
+      renderMovies(movie);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+setPopularMovie();
+
 function renderMovies(movie) {
-  // inputTitle.value = "bond";
   console.log(movie.results);
   console.log(inputTitle.value);
   movie.results.forEach(
@@ -47,15 +66,19 @@ function renderMovies(movie) {
       overview,
       popularity,
       poster_path,
-      relase_date,
+      release_date,
       backdrop_path,
+      genre_ids,
+      genres,
     }) => {
+      // console.log(genre_ids[1]);
+      let relaseYear = release_date.substring(0, 4);
       filmGallery.innerHTML += `
       <ul class="film-list">
         <div class="film-card">        
           <img class="film-cover" src="${baseURL}${posterSize}${poster_path}" alt="${original_title}" loading="lazy" />
           <p class="film-title">${original_title}</p>
-          <p class="film-info">${id}</p>
+          <p class="film-info"> | ${relaseYear}</p>
         </div>
       </ul>`;
     }
@@ -64,9 +87,13 @@ function renderMovies(movie) {
 
 const newSearch = (e) => {
   e.preventDefault();
+
+  if (inputTitle.value === "") {
+    setPopularMovie();
+  }
+
   filmGallery.innerHTML = "";
   searchBoxValue();
 };
 
-inputTitle.addEventListener("click", newSearch);
-
+inputTitle.addEventListener("change", newSearch);
