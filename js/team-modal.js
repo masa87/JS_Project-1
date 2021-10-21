b0e1c2220a442d4d70525a26eca1d26ccb567fff
Artur Kraśniewski <artur.krasniewski1@gmail.com>
2021-10-21 22:46:45 +0200

    Dodany skrypt do okna modalnego z teamem
    i drobne poprawki w stylowaniu okna

---
+++ js/team-modal.js
@@ -0,0 +1,14 @@
+const qs = (sel) => document.querySelector(sel);
+
+const openModal = qs("[data-open-team]");
+const closeModal = qs("[data-close-team]");
+const modal = qs("[data-backdrop]");
+console.log(openModal);
+console.log(closeModal);
+
+const toggleModal = () => {
+  modal.classList.toggle("team-is-hidden");
+};
+
+openModal.addEventListener("click", toggleModal);
+closeModal.addEventListener("click", toggleModal);
--- js/trendMovies.js
+++ js/trendMovies.js
@@ -1,13 +1,13 @@
-import { fetchPopularMovie } from "./fetchApi";
-import { renderMovies } from "./renderMovies";
+// import { fetchPopularMovie } from "./fetchApi";
+// import { renderMovies } from "./renderMovies";
 
-export default function setPopularMovie() {
-  fetchPopularMovie()
-    .then((movie) => {
-      renderMovies(movie);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-}
+// export default function setPopularMovie() {
+//   fetchPopularMovie()
+//     .then((movie) => {
+//       renderMovies(movie);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// }
 
