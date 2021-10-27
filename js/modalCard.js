5a3e6dfe668ed92040140c37aecc1a350f75d074
Artur Kraśniewski <artur.krasniewski1@gmail.com>
2021-10-27 18:58:51 +0200

    formatowanie okna modalnego

--- js/modalCard.js
+++ js/modalCard.js
@@ -9,62 +9,57 @@ let filmId = null;
 const renderSingleMovieCard = (id) => {
   template.innerHTML = `
   <div class="modal__poster-tmp js-poster">
-  <!-- tutaj posterCard -->
-  <!-- Poster do okna modalnego  -->
-<div class="modal__poster">
-<img class="modal__poster-img" src="#" alt="title of poster" />
-</div>
+            <!-- Poster do okna modalnego  -->
+    <div class="modal__poster">
+        <img class="modal__poster-img" src="#" alt="title of poster" />
+    </div>
 </div>
 <div class="modal__info-container">
-  <div class="js-modal">
-    <!-- tutaj będzie CardInfo -->
-    <!-- Szablon karty okno modalne -->
-<div class="modal__descr">
-<h2 class="modal__descr-title">Title</h2>
-<ul class="list modal__descr-list">
+    <div class="js-modal">
+              <!-- Szablon karty okno modalne -->
+        <div class="modal__descr">
+            <h2 class="modal__descr-title">Title</h2>
+            <ul class="list modal__descr-list">
 
-<ul class="list modal__descr-sublist">
-  <li class="modal__descr-details modal__descr-details--indent">Vote / Votes</li>
-  <li class="modal__descr-item">
-      <span class="modal__descr-vote">Rating</span>
-      <span class="modal__descr-separator">/</span>
-      <span class="modal__descr-votes">Votes</span>
-  </li>
-</ul>
+                <ul class="list modal__descr-sublist">
+                    <li class="modal__descr-details modal__descr-details--indent">Vote / Votes</li>
+                    <li class="modal__descr-item">
+                        <span class="modal__descr-vote">Rating</span>
+                        <span class="modal__descr-separator">/</span>
+                        <span class="modal__descr-votes">Votes</span>
+                    </li>
+                </ul>
 
-<ul class="list modal__descr-sublist">
-  <li class="modal__descr-details modal__descr-details--indent">Popularity</li>
-  <li class="modal__descr-item">1163.098</li>
-</ul>
+                <ul class="list modal__descr-sublist">
+                    <li class="modal__descr-details modal__descr-details--indent">Popularity</li>
+                    <li class="modal__descr-item">1163.098</li>
+                </ul>
 
-<ul class="list modal__descr-sublist">
-  <li class="modal__descr-details modal__descr-details--indent">Original Title</li>
-  <li class="modal__descr-item modal__descr-item--title">
-      title
-  </li>
-</ul>
+                <ul class="list modal__descr-sublist">
+                    <li class="modal__descr-details modal__descr-details--indent">Original Title</li>
+                    <li class="modal__descr-item modal__descr-item--title">title</li>
+                </ul>
 
-<ul class="list modal__descr-sublist">
-  <li class="modal__descr-details modal__descr-details--indent">Genre</li>
-  <li class="modal__descr-item">Żenres</li>
-  </li>
-</ul>
+                <ul class="list modal__descr-sublist">
+                    <li class="modal__descr-details modal__descr-details--indent">Genre</li>
+                    <li class="modal__descr-item">Żenres</li>
+                </li>
+                </ul>
 
-</ul>
-<h3 class="modal__descr-subtitle">ABOUT</h3>
-<p class="modal__descr-info">
-About
-</p>
-</div>
-<div class="modal__buttons">
-<button  type="submit" class="js-modal-btn-watched modal__buttons-watched" id="#">
-ADD TO WATCHED
-</button>
-<button type="submit" class="js-modal-btn-queue modal__buttons-queue" id="#">
-ADD TO QUEUE
-</button>
-</div>
-  </div>
+            </ul>
+            <h3 class="modal__descr-subtitle">ABOUT</h3>
+            <p class="modal__descr-info">About</p>
+        </div>
+
+        <div class="modal__buttons">
+            <button  type="submit" class="js-modal-btn-watched modal__buttons-watched" id="#">
+                ADD TO WATCHED
+            </button>
+            <button type="submit" class="js-modal-btn-queue modal__buttons-queue" id="#">
+                ADD TO QUEUE
+            </button>
+        </div>
+    </div>
 </div>
   `
 
