7e782ad759982363eb220b82e4ab5d5335d130c6
Artur Kraśniewski <artur.krasniewski1@gmail.com>
2021-10-27 19:14:49 +0200

    .

--- js/team-modal.js
+++ js/team-modal.js
@@ -3,8 +3,6 @@ const qs = (sel) => document.querySelector(sel);
 const openModal = qs("[data-open-team]");
 const closeModal = qs("[data-close-team]");
 const modal = qs("[data-backdrop]");
-//console.log(openModal);
-//console.log(closeModal);
 
 const toggleModalTeam = () => {
   modal.classList.toggle("team-is-hidden");
