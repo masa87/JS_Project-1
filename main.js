82a1becee530d35c99234f6b81c877c22e8bcf8e
Tomek <61616164+masa87@users.noreply.github.com>
2021-10-30 20:59:05 +0200

    separate js files

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-// import { fetchGenres, fetchMovie, fetchPopularMovie } from "./js/fetchApi";
-// import { renderMovies } from "./js/renderMovies";
-// import { searchBoxValue, setPopularMovie, startPage } from "./js/initFunctions";
-// import { renderPagination, nextPagePagination } from "./js/pagination";
-// import { searchGenres } from "./js/genres";
+import { fetchGenres, fetchMovie, fetchPopularMovie } from "./js/fetchApi";
+import { renderMovies } from "./js/renderMovies";
+import { searchBoxValue, setPopularMovie, startPage } from "./js/initFunctions";
+import { renderPagination, nextPagePagination } from "./js/pagination";
+import { searchGenres } from "./js/genres";
 // import { spinner, body } from "./js/spinner.js";
 
 const qs = (selector) => document.querySelector(selector);
@@ -11,250 +11,250 @@ const filmList = qs(".film-list");
 const paginationContainer = qs(".pagination");
 const paginationitem = document.getElementsByClassName("page-item");
 
-let movieId = [];
-let page = 1;
-let totalPages = 0;
-let currentPage = 1;
-let tempInputValue = "";
+// let movieId = [];
+// let page = 1;
+// let totalPages = 0;
+// let currentPage = 1;
+// let tempInputValue = "";
 
-// -------------------fetch API
+// // -------------------fetch API
 
-async function fetchMovie(title, page) {
-  // spinner.spin(body);
-  try {
-    const response = await fetch(
-      `https://api.themoviedb.org/3/search/movie?api_key=b8c69e73ca2b06d4109ce06d6df842ad&query=${title}&page=${page}`
-    );
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return await response.json();
-  } catch (err) {
-    return console.log(err);
-  }
-}
+// async function fetchMovie(title, page) {
+//   // spinner.spin(body);
+//   try {
+//     const response = await fetch(
+//       `https://api.themoviedb.org/3/search/movie?api_key=b8c69e73ca2b06d4109ce06d6df842ad&query=${title}&page=${page}`
+//     );
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+//     return await response.json();
+//   } catch (err) {
+//     return console.log(err);
+//   }
+// }
 
-// ----------------wyświetlanie topRated filmów
-async function fetchPopularMovie(page) {
-  try {
-    const response = await fetch(
-      `https://api.themoviedb.org/3/trending/movie/week?api_key=b8c69e73ca2b06d4109ce06d6df842ad&page=${page}`
-    );
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return await response.json();
-  } catch (err) {
-    return console.log(err);
-  }
-}
+// // ----------------wyświetlanie topRated filmów
+// async function fetchPopularMovie(page) {
+//   try {
+//     const response = await fetch(
+//       `https://api.themoviedb.org/3/trending/movie/week?api_key=b8c69e73ca2b06d4109ce06d6df842ad&page=${page}`
+//     );
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+//     return await response.json();
+//   } catch (err) {
+//     return console.log(err);
+//   }
+// }
 
-// ------------genres
+// // ------------genres
 
-async function fetchGenres() {
-  // spinner.spin(body);
-  try {
-    const response = await fetch(
-      `https://api.themoviedb.org/3/genre/movie/list?api_key=b8c69e73ca2b06d4109ce06d6df842ad`
-    );
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  } catch (err) {
-    return console.log(err);
-  }
-}
+// async function fetchGenres() {
+//   // spinner.spin(body);
+//   try {
+//     const response = await fetch(
+//       `https://api.themoviedb.org/3/genre/movie/list?api_key=b8c69e73ca2b06d4109ce06d6df842ad`
+//     );
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+//     return response.json();
+//   } catch (err) {
+//     return console.log(err);
+//   }
+// }
 
-function searchGenres() {
-  fetchGenres().then((id) => {
-    return (movieId = id.genres);
-  });
-}
+// function searchGenres() {
+//   fetchGenres().then((id) => {
+//     return (movieId = id.genres);
+//   });
+// }
 
-searchGenres();
+// searchGenres();
 
-// -----------renderowanie listy filmów
-const baseURL = "http://image.tmdb.org/t/p/";
-const posterSize = "w500";
+// // -----------renderowanie listy filmów
+// const baseURL = "http://image.tmdb.org/t/p/";
+// const posterSize = "w500";
 
-function renderMovies(movie) {
-  totalPages = movie.total_pages;
+// function renderMovies(movie) {
+//   totalPages = movie.total_pages;
 
-  renderPagination();
-  movie.results.forEach(
-    ({
-      id,
-      original_title,
-      overview,
-      popularity,
-      poster_path,
-      release_date,
-      backdrop_path,
-      genre_ids,
-    }) => {
-      let getNewId = movieId
-        .filter((genre) => genre_ids.includes(genre.id))
-        .map((genre) => genre.name)
-        .join(", ");
+//   renderPagination();
+//   movie.results.forEach(
+//     ({
+//       id,
+//       original_title,
+//       overview,
+//       popularity,
+//       poster_path,
+//       release_date,
+//       backdrop_path,
+//       genre_ids,
+//     }) => {
+//       let getNewId = movieId
+//         .filter((genre) => genre_ids.includes(genre.id))
+//         .map((genre) => genre.name)
+//         .join(", ");
 
-      let relaseYear = release_date.substring(0, 4);
-      filmList.innerHTML += `
-      <li class="film-list-item">
-        <div class="film-card" data-id="${id}">
-          <img class="film-cover" src="${baseURL}${posterSize}${poster_path}" alt="${original_title}" loading="lazy" />
-          <p class="film-title">${original_title}</p>
-          <p class="film-info">${getNewId} | ${relaseYear}</p>
-        </div>
-      </li>`;
-    }
-  );
-  // spinner.stop();
-}
+//       let relaseYear = release_date.substring(0, 4);
+//       filmList.innerHTML += `
+//       <li class="film-list-item">
+//         <div class="film-card" data-id="${id}">
+//           <img class="film-cover" src="${baseURL}${posterSize}${poster_path}" alt="${original_title}" loading="lazy" />
+//           <p class="film-title">${original_title}</p>
+//           <p class="film-info">${getNewId} | ${relaseYear}</p>
+//         </div>
+//       </li>`;
+//     }
+//   );
+//   // spinner.stop();
+// }
 
-// ---------popularne filmy
-function setPopularMovie() {
-  fetchPopularMovie(page)
-    .then((movie) => {
-      renderMovies(movie);
-    })
-    .catch((err) => {});
-}
+// // ---------popularne filmy
+// function setPopularMovie() {
+//   fetchPopularMovie(page)
+//     .then((movie) => {
+//       renderMovies(movie);
+//     })
+//     .catch((err) => {});
+// }
 
-//---------szukajka
-function searchBoxValue() {
-  if (inputTitle.value !== tempInputValue) {
-    page = 1;
-  }
-  fetchMovie(inputTitle.value, page)
-    .then((movie) => {
-      tempInputValue = inputTitle.value;
-      renderMovies(movie);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-}
+// //---------szukajka
+// function searchBoxValue() {
+//   if (inputTitle.value !== tempInputValue) {
+//     page = 1;
+//   }
+//   fetchMovie(inputTitle.value, page)
+//     .then((movie) => {
+//       tempInputValue = inputTitle.value;
+//       renderMovies(movie);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// }
 
-// -------------- paginacja
+// // -------------- paginacja
 
-function renderPagination() {
-  if (page <= 1) {
-    for (let i = page; i < totalPages; i++) {
-      for (let j = page - 1; j < page + 3; j++) {
-        paginationContainer.innerHTML = `
-        <li class="page-item disabled page-item-previous">
-          <a class="page-link" href="#" tabindex="-1">
-            <svg class="page-icon" width="16" height="16">
-              <use href="./images/pagination/arrows.svg#icon-arrow-left"></use>
-            </svg>
-          </a>
-        </li>
-        <li class="page-item active" data=1>
-          <a class="page-link" href="#">1<span class="sr-only">(current)</span></a>
-        </li>
-        <li class="page-item" data=${page + 1}>
-          <a class="page-link" href="#">${page + 1}</a>
-        </li>
-        <li class="page-item" data=${page + 2}>
-          <a class="page-link" href="#">${page + 2}</a>
-        </li>
-        <li class="page-item" data=${page + 3}>
-          <a class="page-link" href="#">${page + 3}</a>
-        </li>
-        <li class="page-item" data=${page + 4}>
-          <a class="page-link" href="#">${page + 4}</a>
-        </li>
-        <li class="page-item disabled hide">
-          <a class="page-link" href="#">...</a>
-        </li>
-        <li class="page-item hide" data=${totalPages}>
-          <a class="page-link" href="#">${totalPages}</a>
-        </li>
-        <li class="page-item" data=${page + 1}>
-          <a class="page-link" href="#">
-            <svg class="page-icon" width="16" height="16">
-                <use href="./images/pagination/arrows.svg#icon-arrow-right"></use>
-            </svg>
-          </a>
-        </li>
-        `;
-      }
-    }
-  } else {
-    for (let i = page; i < totalPages; i++) {
-      for (let j = page - 1; j < page + 3; j++) {
-        paginationContainer.innerHTML = `
-        <li class="page-item page-item-previous" data=${page - 1}>
-          <a class="page-link" href="#" tabindex="-1">
-            <svg class="page-icon" width="16" height="16">
-              <use href="./images/pagination/arrows.svg#icon-arrow-left"></use>
-            </svg>
-          </a>
-        </li>
-        <li class="page-item hide" data=1>
-          <a class="page-link" href="#">1<span class="sr-only">(current)</span></a>
-        </li>
-        <li class="page-item disabled hide">
-          <a class="page-link" href="#">...</a>
-        </li>
-        <li class="page-item" data=${page - 2}>
-          <a class="page-link" href="#">${
-            page - 2
-          }<span class="sr-only">(current)</span></a>
-        </li>
-        <li class="page-item" data=${page - 1}>
-          <a class="page-link" href="#">${page + -1}</a>
-        </li>
-        <li class="page-item active" data=${page}>
-          <a class="page-link" href="#">${page}</a>
-        </li>
-        <li class="page-item" data=${page + 1}>
-          <a class="page-link" href="#">${page + 1}</a>
-        </li>
-        <li class="page-item" data=${page + 2}>
-          <a class="page-link" href="#">${page + 2}</a>
-        </li>
-        <li class="page-item hide disabled">
-          <a class="page-link" href="#">...</a>
-        </li>
-        <li class="page-item hide" data=${totalPages}>
-          <a class="page-link" href="#">${totalPages}</a>
-        </li>
-        <li class="page-item" data=${page + 1}>
-          <a class="page-link" href="#">
-            <svg class="page-icon" width="16" height="16">
-              <use href="./images/pagination/arrows.svg#icon-arrow-right"></use>
-            </svg>
-          </a>
-        </li>
-        `;
-      }
-    }
-  }
-}
+// function renderPagination() {
+//   if (page <= 1) {
+//     for (let i = page; i < totalPages; i++) {
+//       for (let j = page - 1; j < page + 3; j++) {
+//         paginationContainer.innerHTML = `
+//         <li class="page-item disabled page-item-previous">
+//           <a class="page-link" href="#" tabindex="-1">
+//             <svg class="page-icon" width="16" height="16">
+//               <use href="./images/pagination/arrows.svg#icon-arrow-left"></use>
+//             </svg>
+//           </a>
+//         </li>
+//         <li class="page-item active" data=1>
+//           <a class="page-link" href="#">1<span class="sr-only">(current)</span></a>
+//         </li>
+//         <li class="page-item" data=${page + 1}>
+//           <a class="page-link" href="#">${page + 1}</a>
+//         </li>
+//         <li class="page-item" data=${page + 2}>
+//           <a class="page-link" href="#">${page + 2}</a>
+//         </li>
+//         <li class="page-item" data=${page + 3}>
+//           <a class="page-link" href="#">${page + 3}</a>
+//         </li>
+//         <li class="page-item" data=${page + 4}>
+//           <a class="page-link" href="#">${page + 4}</a>
+//         </li>
+//         <li class="page-item disabled hide">
+//           <a class="page-link" href="#">...</a>
+//         </li>
+//         <li class="page-item hide" data=${totalPages}>
+//           <a class="page-link" href="#">${totalPages}</a>
+//         </li>
+//         <li class="page-item" data=${page + 1}>
+//           <a class="page-link" href="#">
+//             <svg class="page-icon" width="16" height="16">
+//                 <use href="./images/pagination/arrows.svg#icon-arrow-right"></use>
+//             </svg>
+//           </a>
+//         </li>
+//         `;
+//       }
+//     }
+//   } else {
+//     for (let i = page; i < totalPages; i++) {
+//       for (let j = page - 1; j < page + 3; j++) {
+//         paginationContainer.innerHTML = `
+//         <li class="page-item page-item-previous" data=${page - 1}>
+//           <a class="page-link" href="#" tabindex="-1">
+//             <svg class="page-icon" width="16" height="16">
+//               <use href="./images/pagination/arrows.svg#icon-arrow-left"></use>
+//             </svg>
+//           </a>
+//         </li>
+//         <li class="page-item hide" data=1>
+//           <a class="page-link" href="#">1<span class="sr-only">(current)</span></a>
+//         </li>
+//         <li class="page-item disabled hide">
+//           <a class="page-link" href="#">...</a>
+//         </li>
+//         <li class="page-item" data=${page - 2}>
+//           <a class="page-link" href="#">${
+//             page - 2
+//           }<span class="sr-only">(current)</span></a>
+//         </li>
+//         <li class="page-item" data=${page - 1}>
+//           <a class="page-link" href="#">${page + -1}</a>
+//         </li>
+//         <li class="page-item active" data=${page}>
+//           <a class="page-link" href="#">${page}</a>
+//         </li>
+//         <li class="page-item" data=${page + 1}>
+//           <a class="page-link" href="#">${page + 1}</a>
+//         </li>
+//         <li class="page-item" data=${page + 2}>
+//           <a class="page-link" href="#">${page + 2}</a>
+//         </li>
+//         <li class="page-item hide disabled">
+//           <a class="page-link" href="#">...</a>
+//         </li>
+//         <li class="page-item hide" data=${totalPages}>
+//           <a class="page-link" href="#">${totalPages}</a>
+//         </li>
+//         <li class="page-item" data=${page + 1}>
+//           <a class="page-link" href="#">
+//             <svg class="page-icon" width="16" height="16">
+//               <use href="./images/pagination/arrows.svg#icon-arrow-right"></use>
+//             </svg>
+//           </a>
+//         </li>
+//         `;
+//       }
+//     }
+//   }
+// }
 
-let currentPageNr = 0;
+// let currentPageNr = 0;
 
-const nextPagePagination = (e) => {
-  filmList.innerHTML = "";
-  currentPageNr = e.target.closest("li");
-  page = parseInt(currentPageNr.getAttribute("data"));
-  console.log(page);
-  if (inputTitle.value === "") {
-    setPopularMovie();
-  }
-  searchBoxValue();
-};
+// const nextPagePagination = (e) => {
+//   filmList.innerHTML = "";
+//   currentPageNr = e.target.closest("li");
+//   page = parseInt(currentPageNr.getAttribute("data"));
+//   console.log(page);
+//   if (inputTitle.value === "") {
+//     setPopularMovie();
+//   }
+//   searchBoxValue();
+// };
 
-function startPage(e) {
-  e.preventDefault();
-  if (inputTitle.value === "") {
-    setPopularMovie();
-  }
-  filmList.innerHTML = "";
-  searchBoxValue();
-}
+// function startPage(e) {
+//   e.preventDefault();
+//   if (inputTitle.value === "") {
+//     setPopularMovie();
+//   }
+//   filmList.innerHTML = "";
+//   searchBoxValue();
+// }
 
 setPopularMovie();
 
